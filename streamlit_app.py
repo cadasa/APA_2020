7529f755b5f526afdb16e471a998a80a855a6c3d
khanh <khanhduc@gmail.com>
2021-01-21 19:53:45 +0100

    vvv

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def main():
     st.title("APA-2020: Awards in Predefined Areas 2020")
     st.sidebar.title("Navigation")
     apa2020()
-    col1, col2,col3 = st.sidebar.beta_columns([0.9,8.1,1])
+    col1, col2,col3 = st.sidebar.beta_columns([0.9,8.2,0.9])
     col2.info(
                     '👉 Click on each company to see all its APA-2020 PL.'
                     ' Individual PL can be selected using the pullout box above.')
