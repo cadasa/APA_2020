bb23f3cf79b56a215b2aecbfdef74e04c41c3b61
khanh <khanhduc@gmail.com>
2021-01-21 19:42:15 +0100

    null

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,6 +66,7 @@ def main():
 def apa2020():
     col1, col2,col3 = st.sidebar.beta_columns([0.9,8.1,1])
     gdf_pl,df_pl,df_coasline_no,gdf_dsc,df_apa = read_fielddata()
+    df_pl = df_pl.loc[df_pl.loc[:,'O/P'].notnull(),:].reset_index(drop=True)
 #    st.dataframe(df_pl)
 #    st.stop()
     plnames = df_pl.drop_duplicates(subset = ['PL'])['PL'].to_list()
